6535d9689db75ef3d2ebf4d910f61a520c1623f1
matzal <mati.zalewski@gmail.com>
2018-05-05 15:05:12 +0200

    generic stack implementation - initial commit

---
+++ src/main/java/jjdz4/zalewski/mateusz/Stack.java
@@ -0,0 +1,10 @@
+package jjdz4.zalewski.mateusz;
+
+public interface Stack<T> {
+
+    void push(T o);
+
+    T pop();
+
+    T peek();
+}
---
+++ src/main/java/jjdz4/zalewski/mateusz/StackImpl.java
@@ -0,0 +1,18 @@
+package jjdz4.zalewski.mateusz;
+
+public class StackImpl<T> implements Stack<T> {
+    @Override
+    public void push(T o) {
+        
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+}
