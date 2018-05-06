6f58c7aeb3aeea404460770a90841a36162e64d4
matzal <mati.zalewski@gmail.com>
2018-05-06 18:32:49 +0200

    implement generic stack

--- src/main/java/jjdz4/zalewski/mateusz/StackImpl.java
+++ src/main/java/jjdz4/zalewski/mateusz/StackImpl.java
@@ -1,18 +1,35 @@
 package jjdz4.zalewski.mateusz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StackImpl<T> implements Stack<T> {
+
+    private List<T> stackList = new ArrayList<>();
+    private int topOfStack = -1;
+
     @Override
     public void push(T o) {
-        
+        stackList.add(o);
+        topOfStack = topOfStack + 1;
     }
 
     @Override
     public T pop() {
-        return null;
+        if (topOfStack < 0) {
+            throw new IllegalStateException();
+        }
+        T objectFromTopOfStack = stackList.get(topOfStack);
+        stackList.remove(topOfStack);
+        topOfStack = topOfStack - 1;
+        return objectFromTopOfStack;
     }
 
     @Override
     public T peek() {
-        return null;
+        if (topOfStack < 0) {
+            throw new IllegalStateException();
+        }
+        return stackList.get(topOfStack);
     }
 }
